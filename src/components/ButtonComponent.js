a1b588a2e7a1c08b92d5d87c31984a0be958f1b9
Medi Risasi <morisasy@gmail.com>
2019-09-13 22:43:28 +0300

    Calculator part 1

---
+++ src/components/ButtonComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Button extends Component {
+  render() {
+    return (
+      <div>
+          <button className= "calc-button">1</button>       
+      </div>
+    );
+  }
+}
+
+export default Button;
